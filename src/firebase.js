ffcf896c14ab8dd66b6d7156ddbede1c9e9f85fa
sadras16888 <sadras16888@gmail.com>
2026-08-18 09:10:46 +0330

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,5 @@
-import { initializeApp } from 'firebase/app';
-import { initializeFirestore } from 'firebase/firestore';
-
 // این چند خط رو با کلیدهایی که از کنسول Firebase گرفتی جایگزین کن.
-// (توضیح کامل مرحله‌به‌مرحله توی فایل README.md هست)
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: 'AIzaSyDcx1AEGSLR7OL2rDBASVUobOBompS9NUM',
   authDomain: 'vernamall-1baf4.firebaseapp.com',
   projectId: 'vernamall-1baf4',
@@ -11,8 +7,3 @@ const firebaseConfig = {
   messagingSenderId: '1092175083848',
   appId: '1:1092175083848:web:079a08a6ea1096d37e5b7d',
 };
-
-const app = initializeApp(firebaseConfig);
-export const db = initializeFirestore(app, {
-  experimentalForceLongPolling: true,
-});
