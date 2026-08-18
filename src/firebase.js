a754b61c1c86a415529ff5813f50391cbc10659b
sadras16888 <sadras16888@gmail.com>
2026-08-18 09:26:49 +0330

    Delete src/firebase.js

--- src/firebase.js
+++
@@ -1,9 +0,0 @@
-// این چند خط رو با کلیدهایی که از کنسول Firebase گرفتی جایگزین کن.
-export const firebaseConfig = {
-  apiKey: 'AIzaSyDcx1AEGSLR7OL2rDBASVUobOBompS9NUM',
-  authDomain: 'vernamall-1baf4.firebaseapp.com',
-  projectId: 'vernamall-1baf4',
-  storageBucket: 'vernamall-1baf4.firebasestorage.app',
-  messagingSenderId: '1092175083848',
-  appId: '1:1092175083848:web:079a08a6ea1096d37e5b7d',
-};
